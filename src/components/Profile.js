4851ec8d1432f1ba4dc18889ff201bc660c001f9
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2020-05-06 18:03:35 +0200

    chore: started with Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,9 +10,32 @@ const Profile = () => {
     
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <div style={{ width: 200, height: 200, background: `url(${context.user.thumbnail})`, backgroundSize: 'cover', borderRadius: '50%' }} />
-
-            Hello <strong>{context.user.name}</strong>! We are really really glad to see your name here
+            <div style={
+                {
+                     width: 200, 
+                     height: 200, 
+                     background: `url(${context.user.thumbnail})`, 
+                     backgroundSize: 'cover', 
+                     borderRadius: '50%' 
+                }
+            } />
+            <div>Hello <strong>{context.user.name}</strong></div>
+            {!context.user.googleId && <div>
+                <h1>Change Password</h1>  
+                <div>
+                    <label>Current Password</label> 
+                    <input type="password" />
+                </div>
+                <div>
+                    <label>New Password</label> 
+                    <input type="password" />
+                </div>
+                <div>
+                    <label>Confirm Password</label> 
+                    <input type="password" />
+                </div>
+                <button>Submit</button>
+            </div>}            
         </div>
     );
 };
