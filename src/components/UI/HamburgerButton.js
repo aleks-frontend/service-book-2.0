cb48eb06a5e5f47bc0eb3f353f668a3d645208c3
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2020-06-08 17:00:34 +0200

    fix: made Hamburger button not selectable on mobile

--- src/components/UI/HamburgerButton.js
+++ src/components/UI/HamburgerButton.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { AppContext } from '../AppProvider';
-
 import { breakpoints, zIndexes } from '../../helpers';
 
 const HamburgerButtonMain = styled.div`
@@ -29,6 +27,7 @@ const HamburgerButtonBar = styled.div`
     background: #fff;
     transition: all 0.3s;
     transform: ${props => props.animated ? 'rotate(135deg)' : 'rotate(0deg)'};
+    user-select: none;
 
     &::after, &::before {
         content: '';
